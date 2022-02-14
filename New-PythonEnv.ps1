43f5d75203439a9f7493720deb0dcd2513779252
Ricky White <ricky@whitelionmedia.com>
2022-02-13 19:24:06 -0500

    Minor refactoring

--- New-PythonEnv.ps1
+++ New-PythonEnv.ps1
@@ -4,20 +4,19 @@
 .DESCRIPTION
     Create a virtual environment with your specified version of Python, and install required packages.
 .EXAMPLE
-    PythonEnv.ps1 -Version 3.9 -Name .venv
+    New-Python.ps1 -Version 3.9
 .EXAMPLE
-    PythonEnv.ps1 -Version 3.9 -Name .venv -RequirementsFile requirements.txt
-.INPUTS
-    Inputs to this cmdlet (if any) #TODO:
-.OUTPUTS
-    Output from this cmdlet (if any) #TODO:
+    New-Python.ps1 -Path path\to\python.exe
+.EXAMPLE
+    New-Python.ps1 -Version 3.9 -Name env -RequirementsFile requirements.txt
 #>
 [CmdletBinding()]
 Param (
-    [Parameter(Mandatory = $true, ParameterSetName = "Version", Position = 0)]
-    [ValidateSet("3.6", "3.7", "3.8", "3.9", "3.10")]
+    [Parameter(Mandatory, ParameterSetName = "Version", Position = 0)]
+    [ValidateSet("3.7", "3.8", "3.9", "3.10")]
     [string] $Version,
-    [Parameter(Mandatory = $true, ParameterSetName = "Path", Position = 0)]
+
+    [Parameter(Mandatory, ParameterSetName = "Path", Position = 0)]
     [string] $PythonPath,
 
     [Parameter(Position = 1)]
@@ -29,30 +28,36 @@ Param (
 
 # Change the ErrorActionPreference to 'Stop'
 $ErrorActionPreference = 'Stop'
-Write-Error -Message $Error[0]
 
-# Find the selected Python executable
-if ($PSCmdlet.ParameterSetName -eq "Version") {
-    $PythonVersion = "Python" + $Version.Replace(".", "")
-    $PythonPath = "C:\Users\$env:USERNAME\AppData\Local\Programs\Python\${PythonVersion}\python.exe"
+try {
+    # Find the selected Python executable
+    if ($PSCmdlet.ParameterSetName -eq "Version") {
+        $PythonVersion = "Python" + $Version.Replace(".", "")
+        $PythonPath = "C:\Users\$env:USERNAME\AppData\Local\Programs\Python\${PythonVersion}\python.exe"
+    }
+    Write-Output "Creating a new virtual environment called $Name, using Python $PythonPath"
+
+    # Create the virtual environment
+    . $PythonPath -m venv $Name
+
+    # Activate the virtual environment
+    Write-Output "Activating $Name..."
+    . $Name\scripts\activate.ps1
+
+    # Upgrade pip
+    Write-Output "Updating pip to latest version..."
+    python -m pip install --upgrade pip
+
+    # If a requirements file is given, install its dependancies
+    if ($RequirementsFile) {
+        Write-Output "Installing dependencies from $RequirementsFile..."
+        python -m pip install -r $RequirementsFile
+    }
+
+    Write-Output "Environment setup complete. Happy coding!"
+
 }
-Write-Progress "Creating a new virtual environment called $Name, using Python $PythonPath"
-
-# Create the virtual environment
-. $PythonPath -m venv $Name
-
-# Activate the virtual environment
-Write-Progress "Activating $Name..."
-. $Name\scripts\activate.ps1
-
-# Upgrade pip
-Write-Progress "Updating pip to latest version..."
-python -m pip install --upgrade pip
-
-# If a requirements file is given, install its dependancies
-if ($RequirementsFile) {
-    Write-Progress "Installing dependencies from $RequirementsFile..."
-    python -m pip install -r $RequirementsFile
+catch {
+    Write-Error -Message $Error[0]
 }
 
-Write-Progress "Environment setup complete. Happy coding!"
